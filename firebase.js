39d8f7023487b233380a068b1abc0536793e41b7
nelson <peternelson063@gmail.com>
2023-07-22 18:47:53 +0100

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3EM-GMcPGualYaxaI7gdcviHAzUnnepw',
+  authDomain: 'renz-ng.firebaseapp.com',
+  projectId: 'renz-ng',
+  storageBucket: 'renz-ng.appspot.com',
+  messagingSenderId: '662591846722',
+  appId: '1:662591846722:web:faa0e63d8ac456471a70db',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import {
+  BsFillChatLeftDotsFill,
+  BsSearch,
+  BsThreeDotsVertical,
+} from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
+
+const Sidebar = () => {
+  return (
+    <aside className='bg-white'>
+      <header className='p-3 flex items-center justify-between h-16 border-b border-solid border-gray-100 z-10 sticky top-0 bg-white'>
+        <FaUserCircle className='text-slate-500 cursor-pointer text-2xl hover:opacity-80 hover:scale-105' />
+        <div className='flex items-center cursor-pointer space-x-4'>
+          <BsFillChatLeftDotsFill className='text-slate-500 hover:text-slate-400' />
+          <BsThreeDotsVertical className='text-slate-500 hover:text-slate-400' />
+        </div>
+      </header>
+      <div className='flex items-center p-3 rounded-md space-x-2'>
+        <BsSearch className='text-gray-500' />
+        <input
+          type='text'
+          placeholder='Search in chats...'
+          className='outline-0 border-none flex-1 placeholder:text-sm placeholder:tracking-widest placeholder:text-gray-600'
+        />
+      </div>
+      <div className='p-1 hover:bg-slate-100 border-b border-t border-gray-100 shadow-sm  transition duration-150 ease-out'>
+        <button className='w-full text-slate-500 tracking-wider uppercase text-sm'>
+          Start a new chat
+        </button>
+      </div>
+      {/* lists of chats */}
+    </aside>
+  );
+};
+export default Sidebar;
